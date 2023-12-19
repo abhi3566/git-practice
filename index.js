ec3a90b4e6166ae5a40343f22502a0b9f6f8f109
abhi3566 <abhi3566@gmail.com>
2023-12-19 18:17:24 +0530

    added  feature 2

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log("this code is of production branch")
-console.log("fetaure added in development branch")
-console.log("more feature added to development branch")
+console.log("feature 1 added in development branch")
+console.log("more features added to development branch feature 2")
